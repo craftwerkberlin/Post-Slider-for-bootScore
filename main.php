0d20285a5f8f3fe1a6b98570f2b955fc9c7b0e08
craftwerk <pst@crftwrk.de>
2020-03-01 08:40:16 +0100

    1.0.0

---
+++ main.php
@@ -0,0 +1,132 @@
+<?php
+/*Plugin Name: Post Slider for bootScore
+Plugin URI: https://bootscore.me
+Description: Post slider for bootScore theme https://bootscore.me. Use Shortcode like this [post-slider type="post" category="sample-category" order="ASC" orderby="title" posts="12"] and read readme.txt in PlugIn folder for options.
+Version: 1.0.0
+Author: Bastian Kreiter
+Author URI: https://crftwrk.de
+License: GPLv2
+*/
+
+
+
+
+
+// Register Styles and Scripts
+function my_scripts() {
+
+    wp_enqueue_script( 'swiper-js', plugins_url( '/js/swiper.min.js', __FILE__ ));
+    
+    wp_enqueue_script( 'custom-swiper-js', plugins_url( '/js/custom.swiper.js', __FILE__ ));
+    
+    wp_register_style( 'swiper', plugins_url('css/swiper.min.css', __FILE__) );
+        wp_enqueue_style( 'swiper' );
+    
+    wp_register_style( 'slider', plugins_url('css/slider.css', __FILE__) );
+        wp_enqueue_style( 'slider' );
+    }
+
+add_action('wp_enqueue_scripts','my_scripts');
+
+
+// Post Slider Shortcode
+add_shortcode( 'post-slider', 'bootscore_post_slider' );
+function bootscore_post_slider( $atts ) {
+	ob_start();
+	extract( shortcode_atts( array (
+		'type' => 'post',
+		'order' => 'date',
+		'orderby' => 'date',
+		'posts' => -1,
+		'category' => '',
+	), $atts ) );
+	$options = array(
+		'post_type' => $type,
+		'order' => $order,
+		'orderby' => $orderby,
+		'posts_per_page' => $posts,
+		'category_name' => $category,
+	);
+	$query = new WP_Query( $options );
+	if ( $query->have_posts() ) { ?>
+
+
+<!-- Swiper -->
+
+<div class="px-5 position-relative my-5">
+
+    <div class="swiper-container">
+
+        <div class="swiper-wrapper">
+
+            <?php while ( $query->have_posts() ) : $query->the_post(); ?>
+
+            <div class="swiper-slide card h-auto mb-5">
+                <!-- Featured Image-->
+                <?php the_post_thumbnail('medium', array('class' => 'card-img-top')); ?>
+
+                <div class="card-body d-flex flex-column">
+                    <div class="mb-2">
+                        <!-- Category Badge -->
+                        <?php
+				            $thelist = '';
+				            $i = 0;
+				            foreach( get_the_category() as $category ) {
+				                if ( 0 < $i ) $thelist .= ' ';
+								    $thelist .= '<a href="' . esc_url( get_category_link( $category->term_id ) ) . '" class="badge badge-secondary">' . $category->name.'</a>';
+								    $i++;
+								}
+								echo $thelist;
+				            ?>
+                    </div>
+                    <!-- Title -->
+                    <h2 class="blog-post-title">
+                        <a href="<?php the_permalink(); ?>">
+                            <?php the_title(); ?>
+                        </a>
+                    </h2>
+                    <!-- Meta -->
+                    <?php if ( 'post' === get_post_type() ) : ?>
+                    <small class="text-secondary mb-2">
+                        <?php
+				            bootscore_date();
+				            bootscore_author();
+				            bootscore_comments();
+				            bootscore_edit();
+				        ?>
+                    </small>
+                    <?php endif; ?>
+                    <!-- Excerpt & Read more -->
+                    <div class="card-text mt-auto">
+                        <?php the_excerpt(); ?> <a class="read-more" href="<?php the_permalink(); ?>"><?php _e('Read more', 'bootscore'); ?></a>
+                    </div>
+                    <!-- Tags -->
+                    <?php bootscore_tags(); ?>
+
+                </div>
+
+            </div>
+
+            <?php endwhile; wp_reset_postdata(); ?>
+            
+        </div> <!-- .swiper-wrapper -->
+        
+        <!-- Add Pagination -->
+        <div class="swiper-pagination"></div>
+        
+    </div><!-- swiper-container -->
+
+    <!-- Add Arrows -->
+    <div class="swiper-button-next"></div>
+    <div class="swiper-button-prev"></div>
+
+</div><!-- px-5 position-relative mb-5 -->
+
+<!-- Swiper End -->
+
+<?php $myvariable = ob_get_clean();
+	return $myvariable;
+	}	
+}
+
+// Post Slider Shortcode End
